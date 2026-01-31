f7986364bd2000bc19b3d4945509863047035599
kishan <ladkishan99@gmail.com>
2026-01-31 11:50:22 +0530

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import RoundedButton from "./components/RoundedButton";
 
 function App() {
   return (
-    <div className="flex justify-center w-full h-screen items-center">
-      <h1 className="text-2xl text-red-600">Tailwind Integrated</h1>
+    <div className="w-full min-h-screen">
+      <Navbar />
+      <div className="w-full bg-gray-100 p-5">
+        <div className="flex justify-around items-center gap-10">
+          <div className="flex gap-5">
+            <RoundedButton value="Men" />
+            <RoundedButton value="Women" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
